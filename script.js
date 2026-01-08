fa6a862daccd1e1b4eaa88205cc1030b11368ab2
turkey888855-cpu <turkey888855@gmail.com>
2026-01-08 03:21:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-const tg = window.Telegram.WebApp;
-tg.expand();
+const home = document.getElementById("home");
+const category = document.getElementById("category");
+const title = document.getElementById("categoryTitle");
 
-function selectTour(tour) {
-  const data = {
-    tour: tour
-  };
+function openCategory(name) {
+  title.innerText = name;
 
-  tg.sendData(JSON.stringify(data));
+  home.classList.add("hidden");
+  category.classList.remove("hidden");
+}
+
+function goBack() {
+  category.classList.add("hidden");
+  home.classList.remove("hidden");
 }
